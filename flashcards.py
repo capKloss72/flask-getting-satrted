a21d0c058d6975b27962772d8a2a886c0f88e1fa
capkloss72 <tym.zon@gmail.com>
2021-08-28 21:30:51 +0800

    Section 3 - Basic Jinja if / for loops

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, jsonify
 from model import db
 
 app = Flask(__name__)
@@ -6,10 +6,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():  # put application's code here
-    return render_template("welcome.html", message="Here is a message from the template")
+    return render_template("welcome.html", cards=db)
 
 
-@app.route('/card')
-def card_view():
-    card = db[1]
-    return render_template("card.html", card=card)
+@app.route('/card/<int:index>')
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card=card, index=index, max_index=len(card))
+    except IndexError:
+        abort(404)
+
+
+@app.route('/api/card')
+def api_card_list():  # put application's code here
+    return jsonify(db)
+
+
+@app.route('/api/card/<int:index>')
+def api_card_details(index):
+    try:
+        card = db[index]
+        return {"card": card}
+    except IndexError:
+        abort(404)
