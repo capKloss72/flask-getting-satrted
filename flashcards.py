7938b53ec569e3045587fedbfcfeb60d247cac7e
capkloss72 <tym.zon@gmail.com>
2021-08-28 18:53:40 +0800

    Section 1 - getting things setup

--- flashcards.py
+++ flashcards.py
@@ -10,6 +10,16 @@ def hello_world():  # put application's code here
     return 'Welcome to my flashcards application!'
 
 
+@app.route('/page_views')
+def get_page_views():  # put application's code here
+    global page_views
+    page_views = page_views + 1
+    return f'This page has been visited: {page_views}'
+
+
+page_views = 0
+
+
 @app.route('/date')
 def get_date():  # put application's code here
-    return f'Welcome to my flashcards application - current date is {datetime.date.today()}'
+    return f"Welcome to my flashcards application - current date is {datetime.date.today()}"
